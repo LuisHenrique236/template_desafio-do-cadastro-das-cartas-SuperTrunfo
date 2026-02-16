a2a8bfb6f3b6d5b95b5ab24e473e811f1c250ad9
LuisHenrique236 <louishenrique236@gmail.com>
2026-02-16 19:34:29 +0000

    cadastros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+int main()
+ char estado;
+ char codigo [20];
+ char nome_cidade [20];
+ int populacao;
+ float area;
+ float pip;
+ int pontos_turisticos;
 
-printf("test")
+ Pintf("Carta 01\n");
+
+
+
 
 return 0;
-} 
+
+}
